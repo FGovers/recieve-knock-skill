d151a25b96d3f8045aac983a3b8288c5974075d9
FGovers <fxgovers@yahoo.com>
2018-06-23 17:07:56 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,18 +4,18 @@ from mycroft import MycroftSkill, intent_file_handler
 class RecieveKnock(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.handle_who=False
 
     @intent_file_handler('knock.recieve.intent')
     def handle_knock_recieve(self, message):
-        self.speak_dialog('knock.recieve')
+        response =self.get_response('who is there')
+        response += " who?"
+        response =self.get_response(response)
+        self.speak_dialog("very_funny")
+        time.sleep(12)
         
-    @intent_file_handler('knock.who.intent').require("name")
-    def handle_who(self,message):
-        utterance=message.data.get('utterance')
-        utterance += " who?"
-        self.speak(utterance)
-        
-    
+    def stop(self):
+        pass
 
 def create_skill():
     return RecieveKnock()
