08823fd8f893d012dfe81953a3161ccf10b7d4ee
FGovers <fxgovers@yahoo.com>
2018-06-23 13:17:47 -0500

    Update __init__.py
    added response to knock knock joke - handle who

--- __init__.py
+++ __init__.py
@@ -8,6 +8,12 @@ class RecieveKnock(MycroftSkill):
     @intent_file_handler('knock.recieve.intent')
     def handle_knock_recieve(self, message):
         self.speak_dialog('knock.recieve')
+        
+    @intent_file_handler('knock.who.intent').require('name')
+    def handle_who(self,message):
+        utterance=message.data.get('utterance')
+        utterance += " who?"
+        self.speak(utterance)
 
 
 def create_skill():
